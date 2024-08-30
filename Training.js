caab70c6c8d9e6a261a6258393b07708e51c2332
RijoCRenny <rijocrenny20132@gmail.com>
2024-08-30 12:54:19 +0530

    Demo_file

---
+++ Training.js
@@ -0,0 +1,176 @@
+// var n1=6;
+// var n2 = 7;
+// var t;
+// console.log("Before Swapping", n1, n2);
+// t=n1;
+// n1=n2;
+// n2=t;
+// console.log("After Swapping", n1,n2);
+// var sum=0;
+// for(i=1;i<=100;i++){
+//     sum=sum+i;
+// }
+// console.log(sum);
+
+// const student={
+//     name:'Monica',
+//     class: 7,
+//     age: 12
+// }
+// console.log(student);
+// console.log(student['age']);
+// for(let i in student){
+//     console.log(`${i} => ${student[i]}`);
+// }
+
+// const students = ['John','Sara', 'Jack'];
+// for (let s of students){
+//     console.log(s[1]);
+// }
+
+
+// const string = 'code';
+// for(let j of string){
+//     console.log(j);
+// }
+// var nu=5;
+// const n=10;
+// var mul;
+// var i=1;
+// do{
+//     mul=nu*i;
+//     i++;
+//     console.log(mul);
+// }while(i<=n);
+
+// let n1 = 48;
+// let n2 = 12;
+// let n3 = 30;
+
+// if(n1>n2 && n1>n3){
+//     console.log(`Largest Number is ${n1}`);
+//     if(n2>n3){
+//         console.log(`2nd Largest Number is ${n2}`);
+//     }
+//     else{
+//         console.log(`3rd Largest Number is ${n3}`);
+//     }
+    
+// }
+// else if(n2>n3 && n2>n1){
+
+// }
+
+// var input = 4;
+// var i = 1;
+// var str = "";
+// while(i<=input){
+//     str = str+input;
+//     console.log(str);
+//     i++;
+// }  
+
+// for (let i = 1; i <=5; i++){
+//     if( i == 3){
+//         continue;
+//     }
+//     console.log(i);
+// } 
+
+// var n1 = 15;
+// var n2 = 25;
+// var cf1 = [];
+// var cf2 = [];
+// var j=0;
+
+// for(i = 1; i<=n1; i++){
+//     if(n1%i==0){
+//         cf1[j]=i;
+//         j++;
+//     }
+// }
+// console.log(cf1);
+
+// var f = 0;
+// for(i = 1; i<=n2; i++){
+//     if(n2%i==0){
+//         cf2[f]=i;
+//         f++;
+//     }
+// }
+// console.log(cf2);
+
+// var hcf=0;
+// for(i = 0; i<=n1;i++){
+//     for(j = 0; j<=n2; j++){
+//         if((cf1[i]==cf2[j])&&(cf1[i]>hcf)){
+//             hcf=cf1[i];
+//         }
+//     }
+// }
+
+// console.log(hcf);
+
+
+// for( row =1; row<=4; row++ ){
+//     str="";
+//     for( col = 1; col<=row; col++ ){
+//         str = str+"*";
+//     }
+//     console.log(str);
+// }
+
+// let sentence = "This is a Car";
+// console.log(sentence);
+
+// let text1 = "This is my \"new\" car";
+// let text2 = "This character \\ is called Backslash";
+// let text3 = "This character \` is called a backtick"
+// console.log(text1);
+// console.log(text2);
+// console.log(text3);
+
+
+// let strings = "I love \ncoding";
+// console.log(strings);
+
+// let sentence2 = "This is my \t new laptop";
+// console.log(sentence2);
+
+// let sentence3 = "This is my \vnew laptop";
+// console.log(sentence3);
+
+
+// const num = 10;
+// const str = "9";
+// console.log(num+str);
+
+// console.log(num+Number(str));
+
+// companyName = "Luminar";
+// console.log(companyName);
+// console.log(companyName.length);
+
+// console.log(companyName[0]);
+
+// console.log(companyName[companyName.length-1]);
+
+// console.log(companyName.charAt(4));
+
+
+// let name1 = "Ann M";
+// let newname = name1.replace("M","Luke");
+// console.log(name1);
+// console.log(newname);
+
+
+// input = 123;
+// str = "";
+// while(input!=0){
+
+//     lastDigit = input%10;;
+//     str = str+lastDigit;
+//     input =Math.floor(input/10);
+// }
+
+
